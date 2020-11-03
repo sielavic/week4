a02efa008f5cbf8c0b888bb9437107d13e1b9c30
sielavic <63396872+sielavic@users.noreply.github.com>
2020-11-04 01:58:36 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+'const CORS = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Methods': 'GET,POST,DELETE',
+  'Access-Control-Allow-Headers': 'Content-Type, Access-Control-Allow-Headers'
+};
+
+const s = require('http').Server((req, res) => {
+    
+    if (req.url==='/result4/'){
+        res.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8', ...CORS});
+        res.write('staskzn\n');
+        
+    }else{
+            res.write('no\n');
+        }
+   
+    
+    res.end();
+});
+s.listen(4321);
