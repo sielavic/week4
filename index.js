301f09f9cb05d7e96ded969dca009bd29e2ba1b5
sielavic <63396872+sielavic@users.noreply.github.com>
2020-11-04 16:14:57 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-'const CORS = {
+const CORS = {
   'Access-Control-Allow-Origin': '*',
   'Access-Control-Allow-Methods': 'GET,POST,DELETE',
   'Access-Control-Allow-Headers': 'Content-Type, Access-Control-Allow-Headers'
