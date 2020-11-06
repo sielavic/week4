fd6ab2e29ff5b388c60f22232b885575babad581
sielavic <63396872+sielavic@users.noreply.github.com>
2020-11-07 01:18:28 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,35 @@
-const CORS = {
-  'Access-Control-Allow-Origin': '*',
-  'Access-Control-Allow-Methods': 'GET,POST,DELETE',
-  'Access-Control-Allow-Headers': 'Content-Type, Access-Control-Allow-Headers'
-};
-
-const s = require('http').Server((req, res) => {
+var http = require('http');
+var CORS = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET,POST,DELETE,OPTIONS',
+    'Access-Control-Allow-Headers': 'Content-Type, Access-Control-Allow-Headers, x-test'
+  };
+  
     
-    if (req.url==='/result4/'){
-        res.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8', ...CORS});
-        res.write('staskzn\n');
-        
-    }else{
-            res.write('no\n');
-        }
-   
+var server = http.createServer(async(req,res)=>{
+      if(req.url === '/result4/'){
+          
     
-    res.end();
-});
-s.listen(4321);
+      res.writeHead(200,{
+              'Content-Type':'application/json',
+              'x-test':"x",
+              ...CORS,
+          })
+          var xTest = req.headers['x-test'];
+          let data = '';
+          await req.on('data', function(chunk){
+            data += chunk;
+            }).on('end', () => {
+          })
+          res.write(JSON.stringify({
+            "message":"staskzn",
+            "x-result":xTest,
+            "x-body":data
+            }
+            ))
+      }
+      res.end()
+      
+  });
+  server.listen(()=>{
+      console.log('Working')});
